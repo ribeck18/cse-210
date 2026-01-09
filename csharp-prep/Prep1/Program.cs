670b31f3f673c710382ff63717de01cddeed693d
ElhokarsWit <beckriley515@gmail.com>
2026-01-08 19:29:36 -0700

    Completed the W01 prove assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
+
+
+
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What percent is your grade? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is a {grade}.");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You have passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the class!");
+        }
     }
 }
